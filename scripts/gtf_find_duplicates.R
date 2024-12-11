a80bdfd5daf66f2d8284a0f5e54e51efe2640814
Kevin Rue-Albrecht <kevinrue@users.noreply.github.com>
2024-12-11 20:23:56 +0000

    fix script to find duplicate gene models

--- scripts/gtf_find_duplicates.R
+++ scripts/gtf_find_duplicates.R
@@ -7,7 +7,7 @@ message("Importing from GTF file ...")
 gtf_data <- import(snakemake@input[["gtf"]])
 message("Done.")
 
-message("GRanges object size: ", format(object.size(sce), unit = "GB"))
+message("GRanges object size: ", format(object.size(gtf_data), unit = "GB"))
 
 message("Extracting gene metadata ...")
 all_genes <- gtf_data %>%
@@ -21,11 +21,15 @@ message("Identifying genes with identical chr/start/end/strand coordinates ...")
 duplicated_gene_boundaries <- all_genes[
   all_genes %>%
     select(seqnames, start, end, strand) %>%
-    duplicated(), ]
+    duplicated(), ] %>%
+    select(seqnames, start, end, strand) %>%
+    unique()
 duplicated_gene_ids <- duplicated_gene_boundaries %>%
   left_join(all_genes, by = join_by(seqnames, start, end, strand)) %>%
   pull(gene_id)
 message("Done.")
+
+message("Duplicated gene boundaries:", format(nrow(duplicated_gene_boundaries), big.mark = ","))
 
 message("Classifying identical/different gene models ...")
 genes_compared <- tibble(
@@ -35,13 +39,13 @@ genes_compared <- tibble(
   secondary_name = character(0),
   status = factor(character(0), c("duplicate", "different"))
 )
-for (i in seq_len(nrow(dup2))) {
+for (i in seq_len(nrow(duplicated_gene_boundaries))) {
   # for (i in 1) {
-  for_seqnames <- dup2 %>% filter(row_number() == i) %>% pull(seqnames)
-  for_start <- dup2 %>% filter(row_number() == i) %>% pull(start)
-  for_end <- dup2 %>% filter(row_number() == i) %>% pull(end)
-  for_strand <- dup2 %>% filter(row_number() == i) %>% pull(strand)
-  for_dup_gene_info <- all_coding_genes %>% filter(
+  for_seqnames <- duplicated_gene_boundaries %>% filter(row_number() == i) %>% pull(seqnames)
+  for_start <- duplicated_gene_boundaries %>% filter(row_number() == i) %>% pull(start)
+  for_end <- duplicated_gene_boundaries %>% filter(row_number() == i) %>% pull(end)
+  for_strand <- duplicated_gene_boundaries %>% filter(row_number() == i) %>% pull(strand)
+  for_dup_gene_info <- all_genes %>% filter(
     seqnames == for_seqnames & start == for_start & end == for_end & strand == for_strand
   ) %>%
     mutate(is_CG = grepl("^CG[[:digit:]]+$", gene_name)) %>%
